7d65f951070b22fd54305941f8eca13455ad40a3
Matteo Muscella <matteo.muscella@mail.com>
2019-07-30 23:08:37 +0200

    Adds basic landing page with instagram link and info.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  margin: 10rem 15px;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  color: #414141;
+`;
+
+const Title = styled.p`
+  margin-bottom: 0;
+  font-size: 34px;
+  font-weight: 500;
+`;
+
+const Subtitle = styled.p`
+  margin-top: 0;
+  font-size: 20px;
+`;
+
+const HashTag = styled.h4`
+  font-weight: 700;
+  font-size: 40px;
+  margin-top: 0;
+`;
+
+const Lander = styled.p`
+  font-weight: 400;
+  font-size: 32px;
+  margin: 2em 0 1em;
+`;
+
+const Anchor = styled.a`
+  color: #414141;
+  text-decoration: none;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Title>Clelia e Tommaso</Title>
+      <Subtitle>06.07.2019</Subtitle>
+      <Lander>That's all folks!</Lander>
+      <Subtitle>Seguici su</Subtitle>
+      <Anchor href={"https://www.instagram.com/explore/tags/lescleins/"}>
+        <HashTag>#lesCleins</HashTag>
+      </Anchor>
+    </Container>
   );
 }
 
