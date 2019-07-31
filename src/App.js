63941937da01f0456540d84006d30f2efd13fdf7
Matteo Muscella <matteo.muscella@mail.com>
2019-07-31 22:17:56 +0200

    Adds bank info.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  margin: 10rem 15px;
+  margin: 7em 6em 10em;
   display: flex;
   flex-direction: column;
   text-align: center;
   color: #414141;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 const Title = styled.p`
@@ -35,21 +37,129 @@ const Lander = styled.p`
 const Anchor = styled.a`
   color: #414141;
   text-decoration: none;
+  
+  :hover {
+    text-decoration: underline;
+  }
 `;
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 50%;
+  align-items: top;
+  justify-content: space-between;
+  margin: 1.5em 0 1em;
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-basis: 100%;
+  flex: 1;
+`;
+
+function Field(props) {
+  return (
+      <>
+        <p className={"text-value"}>{props.value}</p>
+        <p>{props.name}</p>
+      </>
+  );
+}
+
+const marginRightLeft = '20px';
+const StyledField = styled.div`
+  text-align: ${props => props.align};
+  margin-top: 15px;
+  margin-right: ${props => props.align === 'right' ? marginRightLeft : '0'}
+  margin-left: ${props => props.align === 'left' ? marginRightLeft : '0'}
+  .text-value {
+    font-weight: 700;
+    font-size: 1.5em;
+  }
+`;
+
+Field.defaultProps = {
+  align: 'left'
+};
+
+const data = {
+  right: [
+    {
+      value: 'Tommaso Paino',
+      name: 'Intestatario',
+    },
+    {
+      value: 'FR76 3000 4003 6500 0011 8254 748',
+      name: 'IBAN',
+    },
+    {
+      value: 'BNP AFR PP XXX',
+      name: 'BNC',
+    },
+  ],
+  left: [
+    {
+      value: 'Clelia Zurlo',
+      name: 'Intestatario',
+    },
+    {
+      value: 'IT46 N030 6967 6845 1032 6995 226',
+      name: 'IBAN',
+    },
+  ],
+};
 
 function App() {
   return (
-    <Container>
-      <Title>Clelia e Tommaso</Title>
-      <Subtitle>06.07.2019</Subtitle>
-      <Lander>That's all folks!</Lander>
-      <Subtitle>Seguici su</Subtitle>
-      <HashTag>
-        <Anchor href={"https://www.instagram.com/explore/tags/lescleins/"}>
-          #lesCleins
-        </Anchor>
-      </HashTag>
-    </Container>
+      <Container>
+        <Title>Clelia e Tommaso</Title>
+        <Subtitle>06.07.2019</Subtitle>
+        <Lander>That's all folks!</Lander>
+        <Subtitle>Seguici su</Subtitle>
+        <HashTag>
+          <Anchor href={"https://www.instagram.com/explore/tags/lescleins/"}>
+            #lesCleins
+          </Anchor>
+        </HashTag>
+        <Subtitle>E se volete farci un regale, siete ancora in tempo...</Subtitle>
+        <StyledField align={"center"} style={{marginTop: '40px'}}>
+          <Field
+              value={"Regalo Matrimonio"}
+              name={"Causale"}
+          />
+        </StyledField>
+        <Row>
+          <Column>
+            {
+              data['right'].map((d, i) => {
+                return (
+                    <StyledField key={`right-${i}`} align={"right"}>
+                      <Field
+                          value={d.value}
+                          name={d.name}
+                      />
+                    </StyledField>);
+              })
+            }
+          </Column>
+          <Column>
+            {
+              data['left'].map((d, i) => {
+                return (
+                    <StyledField key={`left-${i}`} align={"left"}>
+                      <Field
+                          value={d.value}
+                          name={d.name}
+                      />
+                    </StyledField>);
+              })
+            }
+          </Column>
+        </Row>
+      </Container>
   );
 }
 
